8545194a396a25913dab24457c754eaf7c64c75d
Rafa1123XD <rafael18-carlos@hotmail.com>
2025-05-22 14:40:23 -0300

    mudança de app.json para app.config.js

---
+++ app.config.js
@@ -0,0 +1,60 @@
+/// app.config.js
+export default { 
+    expo: {
+        "name": "projeto-expo-voice",
+        "slug": "projeto-expo-voice",
+        "version": "1.0.0",
+        "orientation": "portrait",
+        "icon": "./assets/images/icon.png",
+        "scheme": "projetoexpovoice",
+        "userInterfaceStyle": "automatic",
+        "newArchEnabled": true,
+        "ios": {
+          "supportsTablet": true,
+          "infoPlist": {
+            "NSMicrophoneUsageDescription": "Este aplicativo precisa acessar o microfone para gravar áudio e medir níveis de som"
+          }
+        },
+        "android": {
+          "adaptiveIcon": {
+            "foregroundImage": "./assets/images/adaptive-icon.png",
+            "backgroundColor": "#ffffff"
+          },
+          "edgeToEdgeEnabled": true,
+          "permissions": [
+            "android.permission.RECORD_AUDIO",
+            "android.permission.MODIFY_AUDIO_SETTINGS"
+          ],
+          "package": "com.anonymous.projetoexpovoice",
+          googleServicesFile: process.env.GOOGLE_SERVICES_JSON || "./google-services.json",
+        },
+        "web": {
+          "bundler": "metro",
+          "output": "static",
+          "favicon": "./assets/images/favicon.png"
+        },
+        "plugins": [
+          "expo-router",
+          [
+            "expo-splash-screen",
+            {
+              "image": "./assets/images/splash-icon.png",
+              "imageWidth": 200,
+              "resizeMode": "contain",
+              "backgroundColor": "#ffffff"
+            }
+          ]
+        ],
+        "experiments": {
+          "typedRoutes": true
+        },
+        "extra": {
+          "router": {},
+          "eas": {
+            "projectId": "6b790217-2851-450e-ad9c-b9cbfd8c0bd6"
+          }
+        },
+        "owner": "rafaelcarlos"
+
+    }
+ }
