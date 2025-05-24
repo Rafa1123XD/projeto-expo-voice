d557635a6220f02691af7bf9ae49e5a19b7c624c
Rafa1123XD <rafael18-carlos@hotmail.com>
2025-05-24 18:23:35 -0300

    Remoção do jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation)',
-  ],
-  setupFiles: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jsdom',
-}; 
